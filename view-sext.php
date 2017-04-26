de668e7fb674fbc66f7c7cb483a1e8cff0d7023d
Sophia Shapira <Sophia.Shapira@gmail.com>
2017-04-25 21:32:39 -0600

    Now all _major_ hours have poetry.

--- view-sext.php
+++ view-sext.php
@@ -1,15 +1,19 @@
-<?php stoic_hour_page_start("Sext"); ?>
-
-
 <?php
+stoic_hour_page_start("Sext");
+
+
 $yetgo = true;
 if ( $yetgo ) { $yegto = stoic_hour_cycle_res(realpath(__DIR__ . '/enchiridion.xml'),2017,5,8,array()); }
 if ( $yetgo ) { $yegto = stoic_hour_cycle_res(realpath(__DIR__ . '/enchiridion.xml'),2017,4,15,array('fixcycle'=>18)); }
-?>
 
-<?php
+
 $yetgo = true;
 if ( $yetgo ) { $yegto = stoic_hour_cycle_res(realpath(__DIR__ . '/eoc-instruct.xml'),2017,4,20,array('chosen-in-ray'=>0)); }
-?>
 
-<?php stoic_hour_page_stop(); ?>
+
+$yetgo = true;
+if ( $yetgo ) { $yegto = stoic_hour_cycle_xres('poetry-main-cycle',2017,4,21,array('fixcycle'=>3)); }
+
+
+stoic_hour_page_stop();
+?>
