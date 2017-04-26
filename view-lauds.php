633d087b25cc15338ef9d8ab21746128a4041258
Sophia Shapira <Sophia.Shapira@gmail.com>
2017-04-25 20:54:52 -0600

    I changed the order in which things appear in Lauds.

--- view-lauds.php
+++ view-lauds.php
@@ -7,12 +7,15 @@ if ( $yetgo ) { $yegto = stoic_hour_cycle_res(realpath(__DIR__ . '/enchiridion.x
 
 
 $yetgo = true;
-if ( $yetgo ) { $yegto = stoic_hour_cycle_xres('poetry-main-cycle',2017,4,20,array()); }
+if ( $yetgo ) { $yegto = stoic_hour_cycle_res(realpath(__DIR__ . '/eoc-instruct.xml'),2017,4,20,array('chosen-in-ray'=>0)); }
 
 
 
 $yetgo = true;
-if ( $yetgo ) { $yegto = stoic_hour_cycle_res(realpath(__DIR__ . '/eoc-instruct.xml'),2017,4,20,array('chosen-in-ray'=>0)); }
+if ( $yetgo ) { $yegto = stoic_hour_cycle_xres('poetry-main-cycle',2017,4,30,array('fixcycle'=>5)); }
+if ( $yetgo ) { $yegto = stoic_hour_cycle_xres('poetry-main-cycle',2017,4,20,array('fixcycle'=>2)); }
+
+
 
 
 stoic_hour_page_stop();
